ff84c037b3a9ec2271190cf041c91f73bd749208
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-02-22 00:13:08 +0000

    crear genero.java

---
+++ src/com/daw/model/Genero.java
@@ -0,0 +1,13 @@
+package com.daw.model;
+
+public enum Genero {
+    NOVELA,
+    CIENCIA_FICCION,
+    HISTORIA,
+    FANTASIA,
+    MISTERIO,
+    INFANTIL,
+    POESIA,
+    THRILLER_PSICOLOGICO,
+    OTRO
+}
