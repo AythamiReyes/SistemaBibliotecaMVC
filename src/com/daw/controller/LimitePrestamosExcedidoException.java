40f7e342c870614eaf7a66d150d90a5adeab57e8
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-02-23 16:54:37 +0000

    añadi super a LimitePrestamosExcedidoException

--- src/com/daw/controller/LimitePrestamosExcedidoException.java
+++ src/com/daw/controller/LimitePrestamosExcedidoException.java
@@ -1,7 +1,5 @@
 package com.daw.controller;
 
 public class LimitePrestamosExcedidoException extends Exception {
-    public LimitePrestamosExcedidoException(String mensaje) {
-        super(mensaje);
-    }
+    public LimitePrestamosExcedidoException(String mensaje) { super(mensaje); }
 }
